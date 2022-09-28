0a2101a7eefed8e1afa1ffb636913535f03b48ce
efuente2 <earnestofuentes@gmail.com>
2022-09-28 13:05:32 -0400

    cleaned up code

--- SpringFrameWorkDemo/src/main/java/org/example/App.java
+++ SpringFrameWorkDemo/src/main/java/org/example/App.java
@@ -1,18 +1,14 @@
 package org.example;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.sql.SQLOutput;
 
 public class App 
 {
     public static void main( String[] args )
     {
-//        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
-//        Student Ernesto = (Student) context.getBean(Student.class);
+
         Student student = (Student) context.getBean("Student");
         System.out.println("Name : " + student.getName() );
         System.out.println("Address : " + student.getAdd());
